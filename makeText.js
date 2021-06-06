435190a27f9931e66ffd4d26fd26ac532b29a102
Robert Long <pongrobertlong@gmail.com>
2021-06-06 00:56:05 -0400

    Second Commit

--- makeText.js
+++ makeText.js
@@ -1,2 +1,44 @@
 /** Command-line tool to generate Markov text. */
+const markov = require('./markov');
+const fs = require('fs');
+const axios = require('axios');
+const process = require('process');
+const type = process.argv[2];
+const content = process.argv[3];
 
+const markovGenerator = (text) => {
+  let mm = new markov.MarkovMachine(text);
+  console.log(mm.makeText());
+};
+
+const makeText = (content) => {
+  fs.readFile(content, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    } else {
+      markovGenerator(data);
+    }
+  });
+};
+
+const urlGenerator = async (content) => {
+  let res;
+
+  try {
+    res = await axios.get(content);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  markovGenerator(res.data);
+};
+
+if (type == 'file') {
+  console.log('Yes');
+  makeText(content);
+} else if (type == 'url') {
+  urlGenerator(content);
+} else {
+  console.process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== '');
     this.makeChains();
   }
 
@@ -18,12 +16,38 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-  }
+    let markovChain = {};
 
+    this.words.forEach((word, index) => {
+      if (!markovChain[word]) {
+        markovChain[word] = [];
+      }
+      markovChain[word].push(
+        word in markovChain ? this.words[index + 1] || null : null
+      );
+    });
+
+    this.chains = markovChain;
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let keys = Object.keys(this.chains);
+    let key = keys[Math.floor(Math.random() * keys.length)];
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key =
+        this.chains[key][Math.floor(Math.random() * this.chains[key].length)];
+    }
+    return output.join(' ');
   }
 }
+
+
+
+module.exports = {
+  MarkovMachine,
+};
